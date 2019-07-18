3bc6c9b8f1dd81ac946c23a93e1b9f13d7a757bb
alexcohn <sasha.cohn@gmail.com>
2019-07-18 17:01:38 +0300

    split libtess to have it build on Windows

--- tess-two/jni/com_googlecode_tesseract_android/Android.mk
+++ tess-two/jni/com_googlecode_tesseract_android/Android.mk
@@ -2,33 +2,20 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libtess
-
-# tesseract (minus executable)
-
-BLACKLIST_SRC_FILES := \
-  %api/tesseractmain.cpp \
-  %viewer/svpaint.cpp
+LOCAL_MODULE := libtess_static1
+LOCAL_THIN_ARCHIVE := true
 
 TESSERACT_SRC_FILES := \
-  $(wildcard $(TESSERACT_PATH)/api/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/arch/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/ccmain/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/ccstruct/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/ccutil/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/classify/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/cube/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/cutil/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/dict/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/lstm/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/neural_networks/runtime/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/opencl/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/textord/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/viewer/*.cpp) \
-  $(wildcard $(TESSERACT_PATH)/wordrec/*.cpp)
 
 LOCAL_SRC_FILES := \
-  $(filter-out $(BLACKLIST_SRC_FILES),$(subst $(LOCAL_PATH)/,,$(TESSERACT_SRC_FILES)))
+  $(subst $(LOCAL_PATH)/,,$(TESSERACT_SRC_FILES))
 
 LOCAL_C_INCLUDES := \
   $(TESSERACT_PATH)/api \
@@ -52,7 +39,7 @@ LOCAL_CFLAGS := \
   -DGRAPHICS_DISABLED \
   --std=c++11 \
   -DUSE_STD_NAMESPACE \
-  -D'VERSION="Android"' \
+  -DVERSION=\"Android\" \
   -include ctype.h \
   -include unistd.h \
   -fpermissive \
@@ -60,7 +47,43 @@ LOCAL_CFLAGS := \
   -Wno-shift-negative-value \
   -D_GLIBCXX_PERMIT_BACKWARD_HASH   # fix for android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/ext/hash_map:61:30: fatal error: backward_warning.h: No such file or directory
 
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+LOCAL_EXPORT_CFLAGS := $(LOCAL_CFLAGS)
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtess_static2
+LOCAL_THIN_ARCHIVE := true
+
+# tesseract (minus executable)
+
+BLACKLIST_SRC_FILES := \
+  %api/tesseractmain.cpp \
+  %viewer/svpaint.cpp
+
+TESSERACT_SRC_FILES := \
+  $(wildcard $(TESSERACT_PATH)/api/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/ccmain/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/lstm/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/neural_networks/runtime/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/opencl/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/textord/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/viewer/*.cpp) \
+  $(wildcard $(TESSERACT_PATH)/wordrec/*.cpp)
+
+LOCAL_SRC_FILES := \
+  $(filter-out $(BLACKLIST_SRC_FILES),$(subst $(LOCAL_PATH)/,,$(TESSERACT_SRC_FILES)))
+
+LOCAL_STATIC_LIBRARIES := libtess_static1
+
+include $(BUILD_STATIC_LIBRARY)
+
 # jni
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libtess
 
 LOCAL_SRC_FILES += \
   pageiterator.cpp \
@@ -76,8 +99,7 @@ LOCAL_LDLIBS += \
 
 # common
 
-LOCAL_PRELINK_MODULE := false
 LOCAL_SHARED_LIBRARIES := liblept
-LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
+LOCAL_STATIC_LIBRARIES := libtess_static1 libtess_static2
 
 include $(BUILD_SHARED_LIBRARY)
