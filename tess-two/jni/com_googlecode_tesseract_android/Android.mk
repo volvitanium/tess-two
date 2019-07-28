ca0702d28ff4affe8f17d36f03d164b467def1c0
alexcohn <sasha.cohn@gmail.com>
2019-07-28 15:22:08 +0300

    cleanup 4.1 build
    dummy DebugPixa will not complain about missing libtiff
    many C flags from older version are not needed anymore

--- tess-two/jni/com_googlecode_tesseract_android/Android.mk
+++ tess-two/jni/com_googlecode_tesseract_android/Android.mk
@@ -29,7 +29,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libtess_core_static
 LOCAL_THIN_ARCHIVE := true
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH) # overriding some tesseract headers
 
 LOCAL_PATH := $(TESSERACT_PATH)
 
@@ -43,6 +43,7 @@ TESSERACT_SRC_FILES := \
   $(wildcard $(TESSERACT_PATH)/dict/*.cpp) \
 
 BLACKLIST_SRC_FILES := \
+    ccutil/fileio.cpp \
     %sse.cpp \
     %avx2.cpp \
     %avx.cpp \
@@ -67,27 +68,19 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_CFLAGS := \
   -DGRAPHICS_DISABLED \
-  --std=c++11 \
-  -DUSE_STD_NAMESPACE \
   -DPACKAGE_VERSION=\"4.1.0\" \
   -DTESSERACT_MAJOR_VERSION=4 \
   -DTESSERACT_MINOR_VERSION=1 \
   -DTESSERACT_MICRO_VERSION=0 \
-  -include ctype.h \
-  -include unistd.h \
-  -fpermissive \
-  -Wno-deprecated \
   -Wno-shift-negative-value
-
-get-src-file-target-cflags += $(if $(filter ccutil/fileio.cpp,$1),-Dglob(a,b,c,d)=0 -Dglobfree(x)=0,)
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 LOCAL_EXPORT_CFLAGS := $(LOCAL_CFLAGS)
-LOCAL_STATIC_LIBRARIES += liblept_static # to inherit C_INCLUDE
+LOCAL_STATIC_LIBRARIES += liblept_static # to inherit C_INCLUDE and CFLAGS
 
 include $(BUILD_STATIC_LIBRARY)
 
-### other static
+### other static (split from _core to keep linker command line less than maximum)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtess_static
@@ -107,6 +100,8 @@ TESSERACT_SRC_FILES := \
   $(wildcard $(TESSERACT_PATH)/viewer/*.cpp) \
   $(wildcard $(TESSERACT_PATH)/wordrec/*.cpp)
 
+LOCAL_PATH := $(TESSERACT_PATH)
+
 LOCAL_SRC_FILES := \
   $(filter-out $(BLACKLIST_SRC_FILES),$(subst $(LOCAL_PATH)/,,$(TESSERACT_SRC_FILES)))
 
@@ -115,12 +110,15 @@ LOCAL_STATIC_LIBRARIES += liblept_static # to inherit C_INCLUDE
 
 include $(BUILD_STATIC_LIBRARY)
 
-### command line
+### command line (statically linked to tesseract libs)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := tesseract
 TESSERACT_SRC_FILES := \
   $(TESSERACT_PATH)/api/tesseractmain.cpp
+
+LOCAL_PATH := $(TESSERACT_PATH)
+
 LOCAL_SRC_FILES := \
   $(subst $(LOCAL_PATH)/,,$(TESSERACT_SRC_FILES))
 
